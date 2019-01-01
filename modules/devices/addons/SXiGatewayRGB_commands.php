c61f788129636f0248d2ebabe5ba4cf33c9ba2fb
Sergey Avdeev <thesoultaker48@gmail.com>
2019-01-02 01:51:38 +0300

    small fixes

--- modules/devices/addons/SXiGatewayRGB_commands.php
+++ modules/devices/addons/SXiGatewayRGB_commands.php
@@ -15,14 +15,14 @@ if ($device_type == 'rgbgt') {
   //$reply_confirm = 1;
  } elseif (preg_match('/' . LANG_COLOR . '/uis', $command)) {
   $colors = array(
-   'ffffff' => array('белый', 'белой', 'белым', 'белом'),
-   'ff0000' => array('красный', 'красной', 'красным', 'красном'),
-   '00ff00' => array('зеленый', 'зеленой', 'зеленым', 'зеленом'),
-   '0000ff' => array('синий', 'синей', 'синим', 'синем'),
+   'ffffff' => array('white', 'бела', 'беле', 'бело', 'белу', 'белы'),
+   'ff0000' => array('red', 'красна', 'красне', 'красно', 'красну', 'красны'),
+   '00ff00' => array('green', 'зелена', 'зелене', 'зелено', 'зелену', 'зелены'),
+   '0000ff' => array('blue', 'сине', 'сини', 'синю', 'синя'),
   );
   foreach($colors as $color_code => $color_words) {
    foreach($color_words as $color_word) {
-    if(preg_match('/' . $color_word . '/uis', $command)) {
+    if(preg_match('/' . preg_quote($color_word, '/') . '/uis', $command)) {
 	 $run_code .= "callMethodSafe('$linked_object.setColor', array('color'=> '$color_code'));";
      $processed = 1;
      $reply_confirm = 1;
