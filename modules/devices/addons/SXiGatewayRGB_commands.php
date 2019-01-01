d7936fc3f7352ac51d735e6c4abe419fd9014551
Sergey Avdeev <thesoultaker48@gmail.com>
2019-01-02 00:15:45 +0300

    Voice commands for color control

--- modules/devices/addons/SXiGatewayRGB_commands.php
+++ modules/devices/addons/SXiGatewayRGB_commands.php
@@ -13,5 +13,22 @@ if ($device_type == 'rgbgt') {
   $opposite_code .= "callMethodSafe('$linked_object.turnOn');";
   $processed = 1;
   //$reply_confirm = 1;
+ } elseif (preg_match('/(.*)' . LANG_COLOR . '(.*)/uis', $command, $matches)) {
+  $colors = array(
+   'ffffff' => array('белый', 'белой', 'белым', 'белом'),
+   'ff0000' => array('красный', 'красной', 'красным', 'красном'),
+   '00ff00' => array('зеленый', 'зеленой', 'зеленым', 'зеленом'),
+   '0000ff' => array('синий', 'синей', 'синим', 'синем'),
+  );
+  foreach($colors as $color_code => $color_words) {
+   foreach($color_words as $color_word) {
+    if(preg_match('/' . $color_word . '/uis', $matches[0])) {
+	 $run_code .= "callMethodSafe('$linked_object.setColor', array('color'=> '$color_code'));";
+     $processed = 1;
+     $reply_confirm = 1;
+	 break 2;
+	}
+   }
+  }
  }
 }
