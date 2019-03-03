738d9cfaf37a70add87a812b1aab612593e256d8
Alexandr-logrus <nechaev.alex@gmail.com>
2019-03-04 01:13:38 +0500

    Исправления, читаем описание
    незначительные правки (обязательна последняя версия МДМ альфа или мастер канал)

--- modules/devices/addons/SXiGatewayRGB_commands.php
+++ modules/devices/addons/SXiGatewayRGB_commands.php
@@ -3,17 +3,19 @@
 if ($device_type == 'rgbgt') {
  if (preg_match('/' . LANG_DEVICES_PATTERN_TURNON . '/uis', $command)) {
   sayReplySafe(LANG_TURNING_ON . ' ' . $device_title . $add_phrase, 2);
-  $run_code .= "callMethodSafe('$linked_object.turnOn');";
-  $opposite_code .= "callMethodSafe('$linked_object.turnOff');";
+  $run_code .= "callMethod('$linked_object.turnOn');";
+  $opposite_code .= "callMethod('$linked_object.turnOff');";
   $processed = 1;
   //$reply_confirm = 1;
- } elseif (preg_match('/' . LANG_DEVICES_PATTERN_TURNOFF . '/uis', $command)) {
+ }
+ elseif (preg_match('/' . LANG_DEVICES_PATTERN_TURNOFF . '/uis', $command)) {
   sayReplySafe(LANG_TURNING_OFF . ' ' . $device_title . $add_phrase, 2);
-  $run_code .= "callMethodSafe('$linked_object.turnOff');";
-  $opposite_code .= "callMethodSafe('$linked_object.turnOn');";
+  $run_code .= "callMethod('$linked_object.turnOff');";
+  $opposite_code .= "callMethod('$linked_object.turnOn');";
   $processed = 1;
   //$reply_confirm = 1;
- } elseif (preg_match('/' . LANG_SXiGatewayRGB_PATTERN_COLOR . '/uis', $command)) {
+ }
+ elseif (preg_match('/' . LANG_SXiGatewayRGB_PATTERN_COLOR . '/uis', $command)) {
   $colors = array(
    'ffffff' => LANG_SXiGatewayRGB_PATTERN_WHITE,
    'ff00ff' => LANG_SXiGatewayRGB_PATTERN_MAGENTA,
@@ -29,13 +31,14 @@ if ($device_type == 'rgbgt') {
   foreach($colors as $color_code => $color_words) {
    if(preg_match('/' . $color_words . '/uis', $command)) {
     //$run_code .= "setGlobal('$linked_object.color', $color_code);";
-    $run_code .= "callMethodSafe('$linked_object.setColor', array('color'=> '$color_code'));";
+    $run_code .= "callMethod('$linked_object.setColor', array('color'=> '$color_code'));";
     $processed = 1;
     $reply_confirm = 1;
     break;
    }
   }
- } elseif (preg_match('/' . LANG_SXiGatewayRGB_PATTERN_BRIGHTNESS . '/uis', $command)) {
+ }
+ elseif (preg_match('/' . LANG_SXiGatewayRGB_PATTERN_BRIGHTNESS . '/uis', $command)) {
   if(preg_match('/(?:\s)(\d{1,2}|100)(?:%|\s|$)/uis', $command, $matches)) {
    $run_code .= "setGlobal('$linked_object.brightness', $matches[1]);";
    $processed = 1;
