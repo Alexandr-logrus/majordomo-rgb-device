759f6feddaac0a3fc67f4348312ab5bce695cac4
Alexandr-logrus <nechaev.alex@gmail.com>
2020-01-05 15:41:46 +0500

    fix cross-linked

--- modules/devices/SXiGatewayRGB_brightnessUpdated.php
+++ modules/devices/SXiGatewayRGB_brightnessUpdated.php
@@ -4,7 +4,9 @@ if ($params['NEW_VALUE'] == $params['OLD_VALUE']) return;
 
 if ($params['NEW_VALUE'] != '0' && $this->getProperty('color') != '000000') {
  $this->setProperty('brightnessSaved', $params['NEW_VALUE']);
- $this->setProperty('status', 1);
+ if ($this->getProperty('status') != 1) {
+  $this->setProperty('status', 1);
+ }
 } else {
  $this->setProperty('status', 0);
 }
--- modules/devices/SXiGatewayRGB_colorUpdated.php
+++ modules/devices/SXiGatewayRGB_colorUpdated.php
@@ -4,7 +4,9 @@ if ($params['NEW_VALUE'] == $params['OLD_VALUE'] || strlen($params['NEW_VALUE'])
 
 if ($params['NEW_VALUE'] != '000000' && $this->getProperty('brightness')) {
  $this->setProperty('colorSaved', $params['NEW_VALUE']);
- $this->setProperty('status', 1);
+ if ($this->getProperty('status') != 1) {
+  $this->setProperty('status', 1);
+ }
 } else {
  $this->setProperty('status', 0);
 }
